7dc84c11ffbea4b3c6e9ff220c4f1f59d56e6446
Student User <student@workstation.lab.example.com>
2022-07-05 00:06:00 -0400

    Frist Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 8080
+const msg = process.env.APP_MSG || 'default message...'
+console.log('port:'+port);
+
+app.get('/', (req, res) => {
+	  res.send(`hello World!, received:${msg}`);
+})
+
+app.get('/healthz', (req, res) => {
+	res.send('Check healthz');
+})
+
+
+
+app.listen(port, () => {
+	  console.log(`Welcome app listening on port ${port}`);
+})
